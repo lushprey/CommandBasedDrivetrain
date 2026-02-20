37d16021fa7c104314772666d926113586bae4cc
EliasA330 <elias.alphox@gmail.com>
2026-02-20 13:44:48 -0600

    Intake System Updated
    Idk if it really works

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,13 +3,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticHu
 
 public class IntakeSubsystem extends SubsystemBase {
 
     private Spark intakeLeftMotor = new Spark(5);
     private Spark intakeRightMotor = new Spark(6);
+    private intakeDoubleSolenoid = new DoubleSolenoid(1,PneumaticsModuleType.REVPH,0,1)
 
     public IntakeSubsystem() {
+        IntakeDoubleSolenoid.set(DoubleSolenoid.Value.kOff);
     }
 
     @Override
@@ -20,9 +26,11 @@ public class IntakeSubsystem extends SubsystemBase {
         if (open) {
             intakeLeftMotor.set(IntakeConstants.kOpenSpeed);
             intakeRightMotor.set(IntakeConstants.kOpenSpeed);
+            intakeDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
         } else {
             intakeLeftMotor.set(IntakeConstants.kCloseSpeed);
             intakeRightMotor.set(IntakeConstants.kCloseSpeed);
+            intakeDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
         }
     }
 
